ea31c6c4092c1803ff330cfb59ea733c5146a38e
Antoan Partadzhiev <a.partadzhiev@nurun.bg>
2019-02-11 12:40:30 +0200

    start kibana after booting vm @ start-services shell script

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -8,3 +8,12 @@ until curl -s http://localhost:9200/_cat/health -o /dev/null; do
     echo Waiting for Elasticsearch to start...
     sleep 3
 done
+
+# Start Kibana
+sudo service kibana start
+
+# Wait for Kibana to start up before doing anything.
+until curl -s http://localhost:5601/ -o /dev/null; do
+    echo Waiting for Kibana...
+    sleep 3
+done
